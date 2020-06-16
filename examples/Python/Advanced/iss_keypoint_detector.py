e1072b2b5bc6757a83c5a2f3f8f7cac88b48d5a5
Ignacio Vizzo <ignaciovizzo@gmail.com>
2020-06-16 13:35:31 -0300

    Update Python Examples and Notebook

--- examples/Python/Advanced/iss_keypoint_detector.py
+++ examples/Python/Advanced/iss_keypoint_detector.py
@@ -19,7 +19,27 @@ def compute_iss_and_visualize(mesh):
 
     tic = time.time()
     keypoints = o3d.keypoints.compute_iss_keypoints(sampled_cloud)
-    print("ISS Computation took {:.0f} [ms]".format(1000 * (time.time() - tic)))
+    toc = 1000 * (time.time() - tic)
+    print("ISS Computation took {:.0f} [ms]".format(toc))
+
+    mesh.compute_triangle_normals()
+    mesh.compute_vertex_normals()
+    mesh.paint_uniform_color([0.5, 0.5, 0.5])
+    keypoints.paint_uniform_color([1.0, 0.75, 0.0])
+    o3d.visualization.draw_geometries([keypoints, mesh])
+
+
+def compute_iss_and_visualize_class(mesh):
+    sampled_cloud = o3d.geometry.PointCloud()
+    sampled_cloud.points = mesh.vertices
+
+    tic = time.time()
+    detector = o3d.keypoints.ISSDetector(sampled_cloud, 0.005, 0.005)
+    detector.gamma_21 = 0.5
+    detector.gamma_32 = 0.5
+    keypoints = detector.compute_keypoints()
+    toc = 1000 * (time.time() - tic)
+    print("ISS Computation took {:.0f} [ms]".format(toc))
 
     mesh.compute_triangle_normals()
     mesh.compute_vertex_normals()
@@ -39,3 +59,6 @@ if __name__ == "__main__":
 
     print("Computing ISS Keypoint on Armadillo model...")
     compute_iss_and_visualize(armadillo)
+
+    print("Computing ISS (with params) Keypoint on Standford Bunny...")
+    compute_iss_and_visualize_class(bunny)
